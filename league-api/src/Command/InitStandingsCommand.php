2bcde4e55f940c236937499b8f9770b2d5788a03
Marcin Sapiela <marcin.sapiela11@gmail.com>
2025-08-07 20:25:10 +0200

    fixing commands

--- league-api/src/Command/InitStandingsCommand.php
+++ league-api/src/Command/InitStandingsCommand.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Command;
 
-use App\Service\TeamStandingInitializer;
+use App\Service\Simulation\TeamStandingInitializer;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 #[AsCommand(
-    name: 'app:init-standings',
+    name: 'league:init-standings',
     description: 'Initializes team standings at the beginning of the season.'
 )]
 class InitStandingsCommand extends Command
